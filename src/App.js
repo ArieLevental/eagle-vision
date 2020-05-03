1339e00464c51e43af754836da6f44a541072775
AverageLeo <Arieka39@gmail.com>
2020-05-03 22:34:24 +0300

    added face recognition component/api & a new logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React , { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Clarifai from 'clarifai';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank';
 import './App.css';
 import Particles from 'react-particles-js';
@@ -21,22 +23,60 @@ const particlesOptions = {
     size: {
       value: 3,
     }
-  },
+  }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles' 
-        params={particlesOptions}
-      />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*<FaceRecognition />*/}     
-    </div>
-  );
+const app = new Clarifai.App({
+ apiKey: 'c63b1f897fdd45e48664cab4ae093598'
+});
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+    app.models
+      .predict(
+        "a403429f2ddf4b49b307e318f00e528b", 
+        "https://samples.clarifai.com/face-det.jpg")
+    .then(
+      function(response) {
+      console.log('worked', response)
+    // do something with responseconsole.log(response);
+      },
+      function(err) {
+      console.log('failed', err)
+    // there was an error
+      }
+    ); 
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles' 
+          params={particlesOptions}
+        />
+        <Navigation /> 
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition />    
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FaceRecognition = () => {
+	return (
+		<div className='center'>
+		<img alt= 'img' src={'https://samples.clarifai.com/face-det.jpg'} />
+		</div>
+	);
+}
+
+export default FaceRecognition;
